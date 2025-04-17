9d171fe587e0f4eea9b5c7b8d948f1f294951616
threev <konitrev@gmail.com>
2025-04-18 01:58:35 +0200

    Changed some constants and had them added to .env instead

--- src/constants.ts
+++ src/constants.ts
@@ -1,10 +1,13 @@
-export const STREAMER_USERNAME = "threevprime";
-export const STREAMER_NAME = "Threev";
-export const USER_ID = "453815125";
+// The username that appears in your URL
+export const STREAMER_USERNAME: string =
+    process.env.STREAMER_USERNAME || "threevprime";
+// The name you want to be called normally
+export const STREAMER_NAME: string = process.env.STREAMER_NAME || "Threev";
+export const USER_ID: string = process.env.USER_ID || "";
 export const OAUTH_TOKEN: string = process.env.ACCESS_TOKEN || "";
 export const CLIENT_ID: string = process.env.CLIENT_ID || "";
+export const DATABASE_FILEPATH: string = process.env.DATABASE_FILEPATH || "";
 export const EVENTSUB_WEBSOCKET_URL = "wss://eventsub.wss.twitch.tv/ws";
-export const DATABASE_FILEPATH = "./database/twitch_database.db";
 export const BANNABLE_SENTENCES = [
     "streamboo .com",
     "streamboo . com",
